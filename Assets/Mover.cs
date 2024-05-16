7ffe6cadb775cdfa48073db09b05874cd536d8a1
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-05-17 00:54:25 +0530

    Mover.cs file updated

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -9,15 +9,24 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Heyy!! Welcome to the Game.");
+    }
+
+    void MovePlayer(){
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
         transform.Translate(xValue,0,zValue);
     }
+
 }
