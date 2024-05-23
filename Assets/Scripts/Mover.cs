97477ed9e1fc6e6dd0dacb4c0be70264748b0aac
Dhruvpopa <202151050@iiitvadodara.ac.in>
2024-05-23 23:40:07 +0530

    Mover Script edited

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,13 +6,11 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.1f;
 
-    // Start is called before the first frame update
     void Start()
     {
         PrintInstructions();
     }
 
-    // Update is called once per frame
     void Update()
     {
         MovePlayer();
